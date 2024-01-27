6bb796d1434ac3915f36d0a11b0d9f4afd17a892
Thiet Doan <thietdv@gmail.com>
2024-01-27 20:29:27 +0700

    Added the first version

---
+++ src/Controller/Index/Index.php
@@ -0,0 +1,42 @@
+<?php
+namespace Thietdv\Hello\Controller\Index;
+
+use Magento\Framework\App\Action\Action;
+use Magento\Framework\App\Action\Context;
+use Magento\Framework\View\Result\PageFactory;
+
+/**
+ * Webkul Hello Landing page Index Controller.
+ */
+class Index extends Action
+{
+    /**
+     * @var PageFactory
+     */
+    protected $_resultPageFactory;
+
+    /**
+     * @param Context     $context
+     * @param PageFactory $resultPageFactory
+     */
+    public function __construct(
+        Context $context,
+        PageFactory $resultPageFactory
+    ) {
+        $this->_resultPageFactory = $resultPageFactory;
+        parent::__construct($context);
+    }
+
+    /**
+     * Hello Landing page.
+     *
+     * @return \Magento\Framework\View\Result\Page
+     */
+    public function execute()
+    {
+        $resultPage = $this->_resultPageFactory->create();
+        // Set title of page
+        $resultPage->getConfig()->getTitle()->set(__('Sample module in Magento 2'));
+        return $resultPage;
+    }
+}
---
+++ src/registration.php
@@ -0,0 +1,6 @@
+<?php
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Thietdv_Hello',
+    __DIR__
+);
